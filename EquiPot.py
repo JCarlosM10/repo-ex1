5aca1454d3e64007c1de9d603aa07041281c91e4
Juan Carlos Martínez Vargas <juancarlos.mv26@gmail.com>
2025-03-28 12:49:22 -0600

    Add files via upload

---
+++ EquiPot.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+valores = np.loadtxt('datos.txt')
+
+x = np.linspace(1, 7, 7)  # Eje x
+y = np.linspace(5, 1, 5)  # Eje y
+X, Y = np.meshgrid(x, y)
+
+plt.figure(figsize=(8, 6))
+plt.contourf(X, Y, valores, cmap='plasma')  
+plt.colorbar(label='Voltaje')
+
+for i in range(len(y)):
+    for j in range(len(x)):
+        plt.text(X[i, j], Y[i, j], f'{valores[i, j]:.2f}', ha='center', va='center', color='black')
+
+plt.title('Voltaje medido en solucion NaCl a 5mm de profundidad (5 Volts)')
+plt.xlabel('Puntos Horizontal')
+plt.ylabel('Puntos Vertical')
+plt.grid()
+plt.show()
